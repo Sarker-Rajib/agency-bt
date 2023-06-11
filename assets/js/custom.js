9a82e04a744310f253c6491a091f6c3b55d9538b
Rajib Sarker <redhat0593@gmail.com>
2023-06-11 19:43:40 +0600

    added more 2 sec

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -87,133 +87,71 @@ pausebtn4.addEventListener('click', () => {
 });
 
 
+{
+    const showcaseToggler1 = () => {
+        const buttons = document.querySelectorAll('.thumb-sec-0');
+        const divs = document.querySelectorAll('.dispay-cards-0');
 
-// thumbs
-const thumb1 = document.getElementById("thumb-1");
-const thumb2 = document.getElementById("thumb-2");
-const thumb3 = document.getElementById("thumb-3");
-const thumb4 = document.getElementById("thumb-4");
-const thumb5 = document.getElementById("thumb-5");
-const thumb6 = document.getElementById("thumb-6");
+        [...buttons].forEach((btn, index) => {
+            btn.addEventListener('click', (e) => {
+                [...buttons].forEach(btn => {
+                    btn.classList.remove('active');
+                })
+                buttons[index].classList.add('active');
 
-// cards
-const card1 = document.getElementById("card-1");
-const card2 = document.getElementById("card-2");
-const card3 = document.getElementById("card-3");
-const card4 = document.getElementById("card-4");
-const card5 = document.getElementById("card-5");
-const card6 = document.getElementById("card-6");
+                [...divs].forEach(btn => {
+                    btn.classList.add('d-none');
+                })
+                divs[index].classList.remove('d-none');
+            })
+        })
+    }
 
-// thumb1 event
-thumb1.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.add('active');
-    thumb2.classList.remove('active');
-    thumb3.classList.remove('active');
-    thumb4.classList.remove('active');
-    thumb5.classList.remove('active');
-    thumb6.classList.remove('active');
+    showcaseToggler1();
+}
 
-    // cards
-    card1.classList.remove('d-none');
-    card2.classList.add('d-none');
-    card3.classList.add('d-none');
-    card4.classList.add('d-none');
-    card5.classList.add('d-none');
-    card6.classList.add('d-none');
-});
+{
+    const showcaseToggler2 = () => {
+        const buttons = document.querySelectorAll('.thumb-sec-1');
+        const divs = document.querySelectorAll('.dispay-cards-1');
 
-// thumb2 event
-thumb2.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.remove('active');
-    thumb2.classList.add('active');
-    thumb3.classList.remove('active');
-    thumb4.classList.remove('active');
-    thumb5.classList.remove('active');
-    thumb6.classList.remove('active');
+        [...buttons].forEach((btn, index) => {
+            btn.addEventListener('click', (e) => {
+                [...buttons].forEach(btn => {
+                    btn.classList.remove('active');
+                })
+                buttons[index].classList.add('active');
 
-    // cards
-    card1.classList.add('d-none');
-    card2.classList.remove('d-none');
-    card3.classList.add('d-none');
-    card4.classList.add('d-none');
-    card5.classList.add('d-none');
-    card6.classList.add('d-none');
-});
+                [...divs].forEach(btn => {
+                    btn.classList.add('d-none');
+                })
+                divs[index].classList.remove('d-none');
+            })
+        })
+    }
 
-// thumb3 event
-thumb3.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.remove('active');
-    thumb2.classList.remove('active');
-    thumb3.classList.add('active');
-    thumb4.classList.remove('active');
-    thumb5.classList.remove('active');
-    thumb6.classList.remove('active');
+    showcaseToggler2();
+}
 
-    // cards
-    card1.classList.add('d-none');
-    card2.classList.add('d-none');
-    card3.classList.remove('d-none');
-    card4.classList.add('d-none');
-    card5.classList.add('d-none');
-    card6.classList.add('d-none');
-});
+{
+    const showcaseToggler3 = () => {
+        const buttons = document.querySelectorAll('.thumb-sec-2');
+        const divs = document.querySelectorAll('.dispay-cards-2');
 
-// thumb4 event
-thumb4.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.remove('active');
-    thumb2.classList.remove('active');
-    thumb3.classList.remove('active');
-    thumb4.classList.add('active');
-    thumb5.classList.remove('active');
-    thumb6.classList.remove('active');
+        [...buttons].forEach((btn, index) => {
+            btn.addEventListener('click', (e) => {
+                [...buttons].forEach(btn => {
+                    btn.classList.remove('active');
+                })
+                buttons[index].classList.add('active');
 
-    // cards
-    card1.classList.add('d-none');
-    card2.classList.add('d-none');
-    card3.classList.add('d-none');
-    card4.classList.remove('d-none');
-    card5.classList.add('d-none');
-    card6.classList.add('d-none');
-});
+                [...divs].forEach(btn => {
+                    btn.classList.add('d-none');
+                })
+                divs[index].classList.remove('d-none');
+            })
+        })
+    }
 
-// thumb5 event
-thumb5.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.remove('active');
-    thumb2.classList.remove('active');
-    thumb3.classList.remove('active');
-    thumb4.classList.remove('active');
-    thumb5.classList.add('active');
-    thumb6.classList.remove('active');
-
-    // cards
-    card1.classList.add('d-none');
-    card2.classList.add('d-none');
-    card3.classList.add('d-none');
-    card4.classList.add('d-none');
-    card5.classList.remove('d-none');
-    card6.classList.add('d-none');
-});
-
-// thumb6 event
-thumb6.addEventListener('click', () => {
-    // thumbs
-    thumb1.classList.remove('active');
-    thumb2.classList.remove('active');
-    thumb3.classList.remove('active');
-    thumb4.classList.remove('active');
-    thumb5.classList.remove('active');
-    thumb6.classList.add('active');
-
-    // cards
-    card1.classList.add('d-none');
-    card2.classList.add('d-none');
-    card3.classList.add('d-none');
-    card4.classList.add('d-none');
-    card5.classList.add('d-none');
-    card6.classList.remove('d-none');
-});
+    showcaseToggler3();
+}
